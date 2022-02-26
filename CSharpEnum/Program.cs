da36de38e1035f1df3649747d06d64dafeabe377
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2022-02-26 18:55:14 +0300

    26/02/2022

---
+++ CSharpEnum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Gunler.Pazar);
+            System.Console.WriteLine((int)Gunler.Cumartesi);
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,Salı,Çarşamba,Perşembe,Cuma,Cumartesi,Pazar
+    }
+}
